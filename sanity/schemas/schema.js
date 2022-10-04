7e416ec97a653dc66eec6e11c645abeeed53f45e
Luis Villalta <luis.villaltar@salud.gob.sv>
2022-10-04 07:36:55 -0600

    Sanity cms

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -2,12 +2,12 @@ import createSchema from "part:@sanity/base/schema-creator";
 
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import blockContent from "./blockContent";
 import category from "./category";
 import restaurant from "./restaurant";
-import author from "./author";
+import dish from "./dish";
+import featured from "./featured";
 
 export default createSchema({
   name: "default",
-  types: schemaTypes.concat([restaurant, author, category, blockContent]),
+  types: schemaTypes.concat([restaurant, dish, category, featured]),
 });
