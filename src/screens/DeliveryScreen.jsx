613c08b6e374de205b38384308bcc0bb286922a2
Luis Villalta <luis.villaltar@salud.gob.sv>
2022-10-06 08:45:00 -0600

    WIP: Delivery Screen

---
+++ src/screens/DeliveryScreen.jsx
@@ -0,0 +1,37 @@
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectRestaurant } from "../features/restaurantSlice";
+import { XMarkIcon } from "react-native-heroicons/solid";
+
+const DeliveryScreen = () => {
+  const navigation = useNavigation();
+  const restaurant = useSelector(selectRestaurant);
+
+  return (
+    <View
+      className={`${
+        Platform.OS === "ios" ? "py-5" : "pt-12"
+      } bg-[#00ccbb] flex-1`}
+    >
+      <SafeAreaView className="z-50">
+        <View className="flex-row justify-between items-center p-5">
+          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+            <XMarkIcon color="white" size={30} />
+          </TouchableOpacity>
+          <Text className="font-light text-white">Order Help</Text>
+        </View>
+
+        <View className="bg-white mx-5 my-2 rounded-md p-6 z-50 shadow-md">
+          <View>
+            <Text className="text-lg text-gray-400">Estimated Arrival</Text>
+            <Text className="text-4xl font-bold">45 - 55 Minutes</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default DeliveryScreen;
